caa04bbaac6c88ace13b3bd948bfeecc81728af6
Zahra Sedighi <zahrasedighi2004@gmail.com>
2026-06-08 12:47:26 +0330

    app icons  was created

--- lib/core/theme/app_icons.dart
+++ lib/core/theme/app_icons.dart
@@ -1,5 +1,7 @@
 
 
+import 'package:flutter/material.dart';
+
 class AppIcons {
   const AppIcons._();
 //pics
@@ -14,6 +16,77 @@ class AppIcons {
   static const String doctor13 = 'assets/pics/Doctor3.svg' ;
   static const String doctor14 = 'assets/pics/Doctor4.svg' ;
   static const String doctor15 = 'assets/pics/Doctor5.svg' ;
+// icons
+  //first row
+ static const IconData person = Icons.person_outline ;
+ static const IconData group = Icons.group_outlined ;
+ static const IconData keyboard = Icons.keyboard_arrow_down_outlined ;
+ static const IconData mic = Icons.mic_none;
+ static const IconData chevronRight = Icons.chevron_right ;
+ static const IconData chevronLeft = Icons.chevron_left ;
+ static const IconData volumeOff = Icons.volume_off_outlined ;
+ static const IconData volumeUp =Icons.volume_up_outlined ;
+ static const IconData stickyNote = Icons.sticky_note_2_outlined ;
+ static const IconData radio = Icons.radio_button_unchecked ;
+
+ // second row
+ static const IconData  location = Icons.location_on_outlined ;
+ static const IconData  chatBubble =  Icons.chat_bubble_outline ;
+ static const IconData  star = Icons.star_border ;
+ static const IconData  favorite = Icons.favorite_border ;
+ static const IconData  thumbUp = Icons.thumb_up_alt_outlined ;
+ static const IconData  home = Icons.home_outlined ;
+ static const IconData  notification = Icons.notifications_none ;
+ static const IconData  grid = Icons.grid_view ;
+ static const IconData  access = Icons.access_time ;
+ static const IconData settings = Icons.settings_outlined ;
+ static const IconData tune = Icons.tune ;
+
+ // third row
+static const IconData calendar = Icons.calendar_month_outlined ;
+static const IconData forum = Icons.forum_outlined ;
+static const IconData call = Icons.call_outlined ;
+static const IconData phone = Icons.phone_forwarded_outlined ;
+static const IconData insert = Icons.insert_chart_outlined ;
+static const IconData description = Icons.description_outlined ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
