3fb8a49aebb5349ba4b581027f8b94ebe1d6abb9
Zahra Sedighi <zahrasedighi2004@gmail.com>
2026-06-10 09:13:40 +0330

    second page view model was created

--- lib/features/secondpage/secondpage_viewmodel/secondpage_viewmodel.dart
+++ lib/features/secondpage/secondpage_viewmodel/secondpage_viewmodel.dart
@@ -1,3 +1,9 @@
 import 'package:get/get.dart';
 
-class SecondpageViewmodel extends GetxController{}
+class SecondpageViewmodel extends GetxController{
+  final selectedCategori = 0.obs ;
+  void changeCategori(int index){
+    selectedCategori.value = index;
+
+  }
+}
